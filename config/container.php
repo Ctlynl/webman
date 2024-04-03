78cabd7dd8f17a3659c3056f104c1c15a6f7b587
make <835215945@qq.com>
2024-04-03 15:55:21 +0800

    添加项目支持依赖注入,使用php-di作为ioc容器

--- config/container.php
+++ config/container.php
@@ -1,3 +1,9 @@
 <?php
 
-return new Webman\Container;
+use DI\ContainerBuilder;
+
+$builder = new ContainerBuilder();
+$builder->addDefinitions(config('dependence', []));
+$builder->useAutowiring(true);
+$builder->useAnnotations(true);
+return $builder->build();
