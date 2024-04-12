c705b02776b1a4478e0fc87bc035c81764c1b630
make <835215945@qq.com>
2024-04-12 18:11:54 +0800

    修改route路由引入规则

--- config/route.php
+++ config/route.php
@@ -9,15 +9,7 @@ foreach (scandir($path) as $item) {
     if ($item != '.' && $item != '..') {
         $fullPath = $path . DIRECTORY_SEPARATOR . $item;
         if (is_file($fullPath)) {
-            include_once $fullPath;
-            continue;
-        }
-        $iterator = new RecursiveDirectoryIterator($fullPath, FilesystemIterator::SKIP_DOTS);
-        $files = new RecursiveIteratorIterator($iterator, RecursiveIteratorIterator::SELF_FIRST);
-        foreach ($files as $file) {
-            if ($file->isFile() && $file->getExtension() == 'php') {
-                include_once $file->getRealPath();
-            }
+            require_once $fullPath;
         }
     }
 }
