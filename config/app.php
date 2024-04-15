72be5312a1360b8b764eade184f04fa3cc8b01fc
make <835215945@qq.com>
2024-04-15 21:02:51 +0800

    添加config app.php 环境变量支持

--- config/app.php
+++ config/app.php
@@ -3,9 +3,9 @@
 use support\Request;
 
 return [
-    'debug' => true,
+    'debug' => filter_var(getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE),
     'error_reporting' => E_ALL,
-    'default_timezone' => 'Asia/Shanghai',
+    'default_timezone' => getenv('APP_TIMEZONE'),
     'request_class' => Request::class,
     'public_path' => base_path() . DIRECTORY_SEPARATOR . 'public',
     'runtime_path' => base_path(false) . DIRECTORY_SEPARATOR . 'runtime',
