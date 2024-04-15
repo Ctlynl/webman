4eb2aacd3c68a74a6c7db37d1ccef548eeb24795
make <835215945@qq.com>
2024-04-15 20:22:23 +0800

    server.php 支持.env 环境变量 因为server.php 不知道动态更新配置 所以没法用nacos

--- config/server.php
+++ config/server.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'listen' => getenv('SERVER_LISTEN'),
-    'transport' => 'tcp',
+    'listen' => getenv('SERVER_HOST') . ':' . getenv('SERVER_PORT'),
+    'transport' => getenv('SERVER_TRANSPORT'),
     'context' => [],
-    'name' => getenv('APP_NAME'),
+    'name' => getenv('SERVER_NAME'),
     'count' => getenv('SERVER_COUNT'),
-    'user' => '',
-    'group' => '',
+    'user' => getenv('SERVER_USER') ?: '',
+    'group' => getenv('SERVER_GROUP') ?: '',
     'reusePort' => false,
     'event_loop' => '',
-    'stop_timeout' => 2,
+    'stop_timeout' => getenv('SERVER_STOP_TIMEOUT'),
     'pid_file' => runtime_path() . '/webman.pid',
     'status_file' => runtime_path() . '/webman.status',
     'stdout_file' => runtime_path() . '/logs/stdout.log',
