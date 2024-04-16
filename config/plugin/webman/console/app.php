5cfbf751d65db506b29728969ec131fbc1866b50
make <835215945@qq.com>
2024-04-16 11:37:58 +0800

    配置webman打包配置文件,添加build文化夹限制提交

--- config/plugin/webman/console/app.php
+++ config/plugin/webman/console/app.php
@@ -1,24 +1,32 @@
 <?php
+
+$myWebManExcludePattern = '#^(?!.*(composer.json|/.github/|/.idea/|/.git/|/.setting/|/runtime/';
+$myWebManExcludePattern .= '|/vendor-bin/|/build/|/vendor/webman/admin/|LICENSE|\.gitignore';
+$myWebManExcludePattern .= '|\.DS_Store|.*\.md|.*\.rst))(.*)$#';
+
 return [
     'enable' => true,
 
-    'build_dir'  => BASE_PATH . DIRECTORY_SEPARATOR . 'build',
+    'build_dir' => BASE_PATH . DIRECTORY_SEPARATOR . 'build',
 
     'phar_filename' => 'webman.phar',
 
     'bin_filename' => 'webman.bin',
 
-    'signature_algorithm'=> Phar::SHA256, //set the signature algorithm for a phar and apply it. The signature algorithm must be one of Phar::MD5, Phar::SHA1, Phar::SHA256, Phar::SHA512, or Phar::OPENSSL.
+    // set the signature algorithm for a phar and apply it.
+    // The signature algorithm must be one of Phar::MD5, Phar::SHA1, Phar::SHA256, Phar::SHA512, or Phar::OPENSSL.
+    'signature_algorithm' => Phar::SHA256,
 
-    'private_key_file'  => '', // The file path for certificate or OpenSSL private key file.
+    // The file path for certificate or OpenSSL private key file.
+    'private_key_file' => '',
 
-    'exclude_pattern'   => '#^(?!.*(composer.json|/.github/|/.idea/|/.git/|/.setting/|/runtime/|/vendor-bin/|/build/|/vendor/webman/admin/))(.*)$#',
+    'exclude_pattern' => $myWebManExcludePattern,
 
-    'exclude_files'     => [
-        '.env', 'LICENSE', 'composer.json', 'composer.lock', 'start.php', 'webman.phar', 'webman.bin'
+    'exclude_files' => [
+        '.env', '.env.example', '.editorconfig', 'LICENSE', 'composer.json', 'composer.lock', 'start.php',
+        'webman.phar', 'webman.bin'
     ],
 
-    'custom_ini' => '
-memory_limit = 256M
-    ',
+    // 打包二进制用的配置
+    'custom_ini' => 'memory_limit = 256M',
 ];
